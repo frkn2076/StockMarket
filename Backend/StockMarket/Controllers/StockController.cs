bbb73593d7866a7b52cd6e6284e0dac554ea437d
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2020-12-20 17:23:03 +0300

    Created backend project

---
+++ Backend/StockMarket/Controllers/StockController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockMarket.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class StockController : ControllerBase {
+
+        private readonly ILogger<StockController> _logger;
+
+        public StockController(ILogger<StockController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get() {
+
+            return string.Empty;
+        }
+    }
+}
